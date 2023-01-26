d6c0c82f48b2921ac38420d82f1c586f4cbb17d0
cvcvcx9 <cvcvcx9@gmaill.com>
2023-01-26 18:19:53 +0900

    SecurityConfig에 설명추가

--- src/main/java/com/cos/security/config/SecurityConfig.java
+++ src/main/java/com/cos/security/config/SecurityConfig.java
@@ -5,13 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
+    //스프링 부트 버젼 2.7이후로는 WebSecurityConfigurerAdapter 상속받아 구현하지 않고,
+    // SecurityFilterChain을 빈등록 하는 방식으로 설정하는 것을 권장하고 있다.
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http
